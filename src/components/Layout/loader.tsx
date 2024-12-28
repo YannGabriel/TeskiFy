36b27cd6fc4d95a0a63a714b5571c5b354da7a29
YannGabriel <yanngabrieldev@gmail.com>
2024-12-27 21:44:14 -0300

    update loader style

--- src/components/Layout/loader.tsx
+++ src/components/Layout/loader.tsx
@@ -1,12 +1,13 @@
 
 
 import loader from "../../images/Loader.svg"
+import styles from "./loader.module.scss"
 
 
 export const Loader = () =>{
   return(
-    <div className="loaderContainer w-[100%] h-[50vh] flex justify-center ">
-      <img src={loader} alt="loading" className="w-[48px] animate-spin360" />
+    <div className={styles.loaderContainer}>
+      <img src={loader} alt="loading" className={styles.loaderIcon} />
     </div>
   )
 }
